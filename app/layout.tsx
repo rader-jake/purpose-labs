600437f49b99e58404e617cce8b18e9a16444a10
Open Claw <openclaw@Mac.lan>
2026-08-20 20:38:33 -0400

    Revert "feat: replace editorial CJC image with live 3D vial viewer"
    This reverts commit a357f70d7f45e7d20d4051779578f64790e6f421.

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body style={{ margin: 0, padding: 0, background: '#0D1B3E' }}>
-        {children}
-      </body>
-    </html>
-  )
-}
