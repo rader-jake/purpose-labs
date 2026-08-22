3bff8dfed3c8ed5c0c6e44112138b3826f886e55
Open Claw <openclaw@Mac-35.lan>
2026-08-21 21:10:09 -0400

    feat: zoom out vial more

--- src/components/VialViewer.tsx
+++ src/components/VialViewer.tsx
@@ -129,7 +129,7 @@ export function VialViewer() {
           rotation-per-second="80deg"
           camera-controls
           disable-zoom
-          camera-orbit="0deg 80deg 12m"
+          camera-orbit="0deg 80deg 18m"
           style={{ width: "100%", height: "100%", minHeight: "480px", background: "transparent" }}
         />
       </div>
