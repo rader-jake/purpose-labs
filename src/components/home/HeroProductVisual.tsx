c7d6a318a1e608100097957d8f94d4db6e8d621d
Open Claw <openclaw@Mac-150.lan>
2026-08-23 04:41:32 -0400

    chore: hide SpinWheel from homepage until ready

--- src/components/home/HeroProductVisual.tsx
+++ src/components/home/HeroProductVisual.tsx
@@ -12,7 +12,8 @@ type HeroProductVisualProps = {
 export function HeroProductVisual(_props: HeroProductVisualProps) {
   return (
     <div className="relative mx-auto select-none" style={{ width: "100%", maxWidth: 480, display: "flex", alignItems: "center", justifyContent: "center" }}>
-      <SpinWheel />
+      {/* SpinWheel hidden until ready — uncomment to re-enable */}
+      {/* <SpinWheel /> */}
     </div>
   );
 }
